59f7cd8ddabdbd01f97957ac4e6ad9b095fdcbb3
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-05-29 19:31:54 +0300

    add import

--- src/employees/EmployeeEdit.tsx
+++ src/employees/EmployeeEdit.tsx
@@ -1,8 +1,20 @@
+import {
+  Edit,
+  SimpleForm,
+  TextInput,
+  SelectInput,
+  NumberInput,
+  BooleanInput,
+  required,
+  minValue,
+  useRecordContext,
+} from "react-admin";
+
 export const EmployeeEdit = () => {
   return (
     <Edit>
       <SimpleForm>
-    
+        <Edit title={<EmployeeTitle />}></Edit>
       </SimpleForm>
     </Edit>
   );
